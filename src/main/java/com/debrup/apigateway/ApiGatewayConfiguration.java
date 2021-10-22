eeb3ebbc88158bfc1b8f97add3137ee2df96c7dc
Debrup Mondal <debrup.official.365@gmail.com>
2021-10-22 16:35:26 +0530

    new route defination added

--- src/main/java/com/debrup/apigateway/ApiGatewayConfiguration.java
+++ src/main/java/com/debrup/apigateway/ApiGatewayConfiguration.java
@@ -23,6 +23,10 @@ public class ApiGatewayConfiguration {
                         .addRequestParameter("Param", "MyValue")
                 )
                 .uri("http://httpbin.org:80"))
+                .route(p-> p
+                        .path("/currency-exchange/**")
+                        .uri("lb://currency-exchange")
+                )
                 .build();
     }
 }
